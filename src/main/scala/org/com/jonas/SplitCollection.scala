e20ad3ac4358d8afa06b749a082e23a883409fd2
jonas <johnnatan999@gmail.com>
2018-04-28 12:30:20 -0500

    Initial Commit

---
+++ src/main/scala/org/com/jonas/SplitCollection.scala
@@ -0,0 +1,48 @@
+package org.com.jonas
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object SplitCollection {
+  /**
+    * @param args
+    * args(0): Path of collection
+    * args(1): Number of files
+    * args(2): Name of files
+    * args(3): Path of result files
+    * @return result
+    *         file with structure: M;k;finalPi,finalA,finalB
+    */
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("akka").setLevel(Level.ERROR)
+
+    val sparkSession = SparkSession.builder.appName("Spark-SplitCollection").getOrCreate()
+    val myCollection: DataFrame = sparkSession.read.csv(args(0))
+
+    val pathFiles = args(3) + "/" + args(2) + "_"
+    (0 to args(1).toInt).foreach(i =>
+      this.writeresult(pathFiles + i.toString, myCollection.schema.toArray.map(e => e.name).mkString(",")))
+
+    var temp = 0
+    myCollection.foreach(r => {
+      if (temp < args(1).toInt) {
+        this.writeresult(pathFiles + temp.toString, r.mkString(","))
+        temp += 1
+      } else {
+        temp = 0
+        this.writeresult(pathFiles + temp.toString, r.mkString(","))
+      }
+    })
+    sparkSession.stop()
+  }
+
+  def writeresult(path: String, result: String): Unit = {
+    import java.io._
+    val fw = new FileWriter(path, true)
+    fw.write(result)
+    fw.close()
+  }
+
+}
